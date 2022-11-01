af1ffb3ff67dcd15927340fbd2098aa6915788bb
Guilherme Franco <nox21@pop-os.localdomain>
2022-11-01 11:40:42 -0300

    Primeiro commit do projeto ATM em java

---
+++ src/ATMCaseStudy.java
@@ -0,0 +1,7 @@
+public class ATMCaseStudy {
+
+    public static void main(String[] args) {
+        ATM theATM = new ATM();
+        theATM.run();
+    }
+}
---
+++ src/BalanceInquiry.java
@@ -0,0 +1,33 @@
+// BalanceInquiry.java
+// Representa uma transação de consulta de saldos do ATM
+
+public class BalanceInquiry extends Transaction {
+
+    public BalanceInquiry( int userAccountNumber, Screen atmScreen, BankDatabase atmBankDatabase ){
+
+        super( userAccountNumber, atmScreen, atmBankDatabase );
+    }// fim do construtor
+
+    // realiza a transação
+    @Override
+    public void execute() {
+        // obtém as referências ao banco de dados e tela do banco
+        BankDatabase bankDatabase = getBankDatabase();
+        Screen screen = getScreen();
+
+        // obtém o saldo disponível da conta envolvida
+        double availableBalance = bankDatabase.getAvailableBalance( getAccountNumber() );
+
+        // obtém o saldo total da conta envolvida
+        double totalBalance = bankDatabase.getTotalBalance( getAccountNumber() );
+
+        // exibe as informações sobre o saldo da tela
+        screen.displayMessageLine( "\nBalance information:" );
+        screen.displayMessage( " - Available balance: " );
+        screen.displayDollarAmout( availableBalance );
+        screen.displayMessage( "\n - Total balance: " );
+        screen.displayDollarAmout( totalBalance );
+        screen.displayMessageLine( "" );
+    }// fim do método execute
+}// fim da classe BalanceInquiry
+
